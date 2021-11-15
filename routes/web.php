c5b5da454a81a395217cad2236ea4cbe4c6a8f2b
febinaa <febinaanjelita@gmail.com>
2021-11-15 16:02:21 +0700

    Database
    PWEB week 12 Database

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,20 @@ Route::post('welcome','ViewController@greetingsphp');
 Route::get('tugasphp','ViewController@formphp');
 
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+
+
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+
+
+Route::post('/pegawai/store','PegawaiController@store');
+
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+
+Route::post('/pegawai/update','PegawaiController@update');
+
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
