8c56bf4c3deceab6dc870e4ba58cbb36ac8062ee
febinaa <febinaanjelita@gmail.com>
2021-11-22 16:18:52 +0700

    mutasi
    Tabel mutasi tugas PWEB

--- resources/views/mutasi/index.blade.php
+++ resources/views/mutasi/index.blade.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 	<title>Tabel Mutasi</title>
 </head>
-<body>
+<body class="container m-6">
 
 	<h3>Data Mutasi</h3>
 
-	<a href="/mutasi/tambah"> + Tambah Pegawai Baru</a>
+	<a href="/mutasi/tambah" type="button" class="btn btn-warning"> + Tambah Pegawai Baru</a>
 
 	<br/>
 	<br/>
 
-	<table border="1">
+	<table class="table table-striped">
 		<tr>
             <th>ID</th>
 			<th>ID Pegawai</th>
@@ -30,14 +35,15 @@
 			<td>{{ $m->SubDepartemen }}</td>
 			<td>{{ $m->MulaiBertugas }}</td>
 			<td>
-				<a href="/mutasi/edit/{{ $m->ID }}">Edit</a>
+				<a href="/mutasi/edit/{{ $m->ID }}" type="button" class="btn btn-primary">Edit</a>
 				|
-				<a href="/mutasi/hapus/{{ $m->ID }}">Hapus</a>
+				<a href="/mutasi/hapus/{{ $m->ID }}" type="button" class="btn btn-danger">Hapus</a>
 			</td>
 		</tr>
 		@endforeach
 	</table>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
